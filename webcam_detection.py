42d59b710c46ae2a68c37cd67e0dcf085f6c28cc
Amrutha Korumilli <34261526+kamrutha@users.noreply.github.com>
2021-04-29 12:21:29 +0530

    Add files via upload

---
+++ webcam_detection.py
@@ -0,0 +1,98 @@
+import numpy as np
+import cv2
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+
+confidenceThreshold = 0.5
+NMSThreshold = 0.3
+
+modelConfiguration = 'cfg/yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+labelsPath = 'dataset.names'
+labels = open(labelsPath).read().strip().split('\n')
+
+np.random.seed(10)
+COLORS = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+outputLayer = net.getLayerNames()
+outputLayer = [outputLayer[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+video_capture = cv2.VideoCapture(0)
+
+(W, H) = (None, None)
+
+while True:
+    phone = 0
+    p = 0
+    ret, frame = video_capture.read()
+    frame = cv2.flip(frame, 1)
+    if W is None or H is None:
+        (H,W) = frame.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB = True, crop = False)
+    net.setInput(blob)
+    layersOutputs = net.forward(outputLayer)
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layersOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > confidenceThreshold:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY,  width, height) = box.astype('int')
+                x = int(centerX - (width/2))
+                y = int(centerY - (height/2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    #Apply Non Maxima Suppression
+    detectionNMS = cv2.dnn.NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold)
+    if(len(detectionNMS) > 0):
+        for i in detectionNMS.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            color = (255,255,255)
+            '''
+            if labels[classIDs[i]]=="cell phone":
+                #messagebox.showwarning("Warning","Phone Detected!!!!")
+
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                phone = phone + 1
+                
+                text = '{}: {:.4f}'.format(labels[classIDs[i]], confidences[i])
+                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            '''   
+            if labels[classIDs[i]] == "person":
+                p = p + 1
+                pic=frame[y:y+h,x:x+w]
+                cv2.imwrite('pic_{}.png'.format(p),pic)
+                
+                if p >=2:
+                    messagebox.showwarning("Warning","Multiple persons detected !!!!")
+                    
+                
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                text = '{}: {:.4f}'.format(labels[classIDs[i]], confidences[i])
+                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                cv2.putText(frame, "Persons count= "+str(p), (x,y+300),cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            
+    cv2.imshow('Output', frame)
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+
+#Finally when video capture is over, release the video capture and destroyAllWindows
+video_capture.release()
+cv2.destroyAllWindows()
